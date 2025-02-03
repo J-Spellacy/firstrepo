4b37095eb0ae556f35e9d6fe48c3fb442508b01d
J-Spellacy <judespellacy@gmail.com>
2025-02-03 22:32:55 +0000

    Update bare_build.py
    from https://medium.com/towards-data-science/math-neural-network-from-scratch-in-python-d6da9f29ce65
    
    code is copied, as a basis for future work

--- project_work/bare_build.py
+++ project_work/bare_build.py
@@ -1,1 +1,17 @@
-print('hello world')
+## from https://medium.com/towards-data-science/math-neural-network-from-scratch-in-python-d6da9f29ce65
+# Base Class
+
+class layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+        
+    # computes Y of a layer for a given X at that layer
+    def forward_prop(self, input):
+        raise NotImplementedError
+    
+    # computes dE/dX for a given DE/dY and updates parameters
+    def backward_prop(self, output_error, learning_rate):
+        raise NotImplementedError
+    
+    
