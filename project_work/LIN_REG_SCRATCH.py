fe357ecf8471f70b67130295b37ae119864b3681
J-Spellacy <judespellacy@gmail.com>
2025-02-17 21:03:29 +0000

    corrected the function to be appropriately named

--- project_work/LIN_REG_SCRATCH.py
+++ project_work/LIN_REG_SCRATCH.py
@@ -4,36 +4,38 @@ import matplotlib.pyplot as plt
 # change this to be noisy data with a linear pattern
 X = np.random.randint(0, 100, 100)
 Y = np.random.randint(0, 100, 100)
+X.sort()
+Y.sort()
 
 print(X)
 
-def K_neighbor(x, X, k=10):
-    if x < round(k/2):
-        X_sample = X[0:k]
-    elif x+round(k/2) > len(X):
-        X_sample = X[-k:]
+def K_neighbor(y, Y, k=10):
+    if y < round(k/2):
+        Y_sample = Y[0:k]
+    elif y+round(k/2) > len(Y):
+        Y_sample = Y[-k:]
     else:
-        X_sample = X[x-(round(k/2)):x+(round(k/2))]
+        Y_sample = Y[y-(round(k/2)):y+(round(k/2))]
     # print(f'{x}  {X[x]}')
     # print(X_sample)
     
-    return(X_sample)
+    return(Y_sample)
     
 
 # estimates the linear line equation  y = mx + c
 def linear_reg(X, Y):
     # print(len(X))
     # print(range(len(X)))
-    Y_hat = np.zeros_like(X)
+    Y_expected = np.zeros_like(X)
     for i in range(len(X)):
         # print(i)
         # print(j)
-        Y_hat[i] = np.mean(K_neighbor(i, Y))
+        Y_expected[i] = np.mean(K_neighbor(i, Y))
         
-    return Y_hat
+    return Y_expected
 
 
-Y_hat = linear_reg(X, Y)
+Y_expected = linear_reg(X, Y)
 
 # create a scatter plot of X and Y and then add line graph of  linear_reg(X, Y)
 plt.figure(figsize=(10, 6))
@@ -47,7 +49,7 @@ plt.title('Scatter Plot and Linear Regression Line')
 plt.scatter(X, Y, color='blue', label='Data Points')
 
 # Add a line graph with the same x but different y values
-plt.plot(X, Y_hat, color='red', label='Linear Regression Line')
+plt.plot(X, Y_expected, color='red', label='Linear Regression Line')
 # Add a legend
 plt.legend()
 
