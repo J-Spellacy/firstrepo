0eaa85ebb6133ed6035b64d09c7e71b5108531a8
J-Spellacy <judespellacy@gmail.com>
2025-02-20 19:26:13 +0000

    trying to  get sprites to work, small bad program to remove backgrounds

---
+++ project_work/background_remover.py
@@ -0,0 +1,62 @@
+from PIL import Image
+import numpy as np
+
+
+
+
+# loop over heights and at each row loop over the row first from the left then the right
+# make the pixel transparent until the pixel value changes then move on
+def remove_background(img, top_bottom = False):
+    img = img.convert('RGBA') # allows for transparency
+    img_new = img.copy()
+    img_check = Image.new('RGBA', img.size, (255, 255, 255, 255))
+    pixels = img.load()
+    pixels_new = img_new.load()
+    pixels_check = img_check.load()    
+    for y in range(img.height):
+        for x in range(img.width):
+            if x == 0:
+                pixels_new[x, y] = (0, 0, 255, 255)
+            elif pixels[x, y] == pixels[x-1, y]:
+                pixels_new[x, y] = (0, 0, 255, 255)
+            else:
+                pixels_new[x, y] = (0, 0, 255, 255)
+                # pixels_check[x, y] = pixels[x, y]
+                break
+        for x in range(img.width):
+            if x == 0 or x == 1:
+                pixels_new[img.width-(x+1), y] = (0, 0, 255, 255)
+            elif pixels[img.width - x, y] == pixels[img.width-(x+1), y]:
+                pixels_new[img.width - x, y] = (0, 0, 255, 255)
+            else:
+                pixels_new[img.width - x, y] = (0, 0, 255, 255)
+                # pixels_check[img.width - x, y] = pixels[x, y]
+                break
+    if top_bottom:
+        for x in range(img.width):
+            for y in range(img.height):
+                if y == 0:
+                    pixels_new[x, y] = (0, 0, 255, 255)
+                elif pixels[x, y] == pixels[x, y-1]:
+                    pixels_new[x, y] = (0, 0, 255, 255)
+                else:
+                    pixels_new[x, y] = (0, 0, 255, 255)
+                    # pixels_check[x, y] = pixels[x, y]
+                    break
+            for y in range(img.height):
+                if y == 0 or y == 1:
+                    pixels_new[x, img.height - (y+1)] = (0, 0, 255, 255)
+                elif pixels[x,img.height - y] == pixels[x, img.height - (y+1)]:
+                    pixels_new[x, img.height - y] = (0, 0, 255, 255)
+                else:
+                    pixels_new[x, img.height - y] = (0, 0, 255, 255)
+                    # pixels_check[img.width - x, y] = pixels[x, y]
+                    break
+
+    return img_new, img_check
+
+if __name__ == '__main__':
+    img = Image.open(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\queen.png")
+    img_new, img_check = remove_background(img, top_bottom = True)
+    img.show()
+    img_new.show()
--- project_work/chess.py
+++ project_work/chess.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import time
 import sys
 
@@ -6,3 +7,51 @@ import sys
 coding chess game using pygame following this tutorial:https://levelup.gitconnected.com/chess-python-ca4532c7f5a4
 '''
 
+class Square(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Square, self).__init__()
+        
+        self.surf = pygame.Surface((25, 25))
+         
+        self.surf.fill((0, 200, 255))
+        self.rect = self.surf.get_rect()
+        
+
+    
+if __name__ == "__main__":
+    
+
+    
+    
+    # initialize pygame
+    pygame.init()
+    
+    colour = (0,0,255) 
+    w_p_positions = [((i*64)+64, (512-j*64)+64) for i in range(8) for j in range(1,3)]
+    b_p_positions = [((i*64)+64, (512-j*64)) for i in range(8) for j in range(6,8)]
+    
+        
+    print(len(w_p_positions))
+    
+    w_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\pawn.png")
+    b_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\b_pawn.png")
+    
+    # Define the dimensions of screen object
+    screen = pygame.display.set_mode((640, 640))
+    # TITLE OF CANVAS 
+    pygame.display.set_caption("The Board") 
+    exit = False
+    
+    ## running the game
+    while not exit: 
+        screen.fill(colour)
+        for p in w_p_positions[::2]:
+            screen.blit(w_pawn_img, p)
+        for p in b_p_positions[9:16]:
+            screen.blit(b_pawn_img, p)
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                exit = True
+        pygame.display.update() 
+    
+    
