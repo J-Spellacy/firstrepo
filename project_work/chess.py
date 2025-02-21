0b634e364f30c77384e7ec47a733cbad363b6f5f
J-Spellacy <judespellacy@gmail.com>
2025-02-21 18:17:07 +0000

    not much changes as busy but added addresses for the sprite images need to  make this irrespective of the path of the machine

--- project_work/chess.py
+++ project_work/chess.py
@@ -7,6 +7,15 @@ import sys
 coding chess game using pygame following this tutorial:https://levelup.gitconnected.com/chess-python-ca4532c7f5a4
 '''
 
+w_pawn, b_pawn = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/pawn_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_pawn_no_bg.png")
+w_bishop, b_bishop = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/bishop_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_bishop_no_bg.png")
+w_knight, b_knight = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/knight_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_knight_no_bg.png")
+w_rook, b_rook = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/rook_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_rook_no_bg.png")
+w_queen, b_queen = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/queen_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_queen_no_bg.png")
+w_king, b_king = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/king_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_king_no_bg.png")
+
+
+
 class Square(pygame.sprite.Sprite):
     def __init__(self):
         super(Square, self).__init__()
@@ -17,21 +26,20 @@ class Square(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
         
 
-    
-if __name__ == "__main__":
-    
+def board_start(positions):
+    for i in positions:
+        if i[0] == 128:
+            break
+    return
+
 
     
-    
+if __name__ == "__main__":
     # initialize pygame
     pygame.init()
     
     colour = (0,0,255) 
-    w_p_positions = [((i*64)+64, (512-j*64)+64) for i in range(8) for j in range(1,3)]
-    b_p_positions = [((i*64)+64, (512-j*64)) for i in range(8) for j in range(6,8)]
-    
-        
-    print(len(w_p_positions))
+    board_positions = [((i*64)+64, (512-j*64)+64) for i in range(8) for j in range(1,9)]
     
     w_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\queen_no_bg.png")
     b_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\b_queen_no_bg.png")
@@ -45,9 +53,10 @@ if __name__ == "__main__":
     ## running the game
     while not exit: 
         screen.fill(colour)
-        for p in w_p_positions[::2]:
-            screen.blit(w_pawn_img, p)
-        for p in b_p_positions[9:16]:
+        # for p in board_positions[::2]:
+        #     screen.blit(w_pawn_img, p)
+        # board set up (initial positions)
+        for p in board_positions:
             screen.blit(b_pawn_img, p)
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
