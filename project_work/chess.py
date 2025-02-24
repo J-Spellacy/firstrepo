cf2805d805da8f60e83d96f3d26941ce5ee5c985
J-Spellacy <judespellacy@gmail.com>
2025-02-24 22:32:55 +0000

    ADDED STARTING FORMAT FOR CLASSES

--- project_work/chess.py
+++ project_work/chess.py
@@ -7,12 +7,12 @@ import sys
 coding chess game using pygame following this tutorial:https://levelup.gitconnected.com/chess-python-ca4532c7f5a4
 '''
 
-w_pawn, b_pawn = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/pawn_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_pawn_no_bg.png")
-w_bishop, b_bishop = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/bishop_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_bishop_no_bg.png")
-w_knight, b_knight = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/knight_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_knight_no_bg.png")
-w_rook, b_rook = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/rook_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_rook_no_bg.png")
-w_queen, b_queen = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/queen_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_queen_no_bg.png")
-w_king, b_king = pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/king_no_bg.png"), pygame.image.load("C:/Users/User/Documents/GitHub/firstrepo/project_work/b_king_no_bg.png")
+w_pawn, b_pawn = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
+w_bishop, b_bishop = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
+w_knight, b_knight = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
+w_rook, b_rook = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
+w_queen, b_queen = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
+w_king, b_king = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png"), pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
 
 
 
@@ -26,41 +26,53 @@ class Square(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
         
 
-def board_start(positions):
-    for i in positions:
-        if i[0] == 128:
-            break
-    return
+class board():
+    def __init__(self):
+        self.positions = [(i*64, j*64) for i in range(1,9) for j in range(1,9)]
+        for i in self.positions:
+            if i[0] == 128:
+                break
+        return
 
 
     
 if __name__ == "__main__":
     # initialize pygame
     pygame.init()
+    i = 0
     
-    colour = (0,0,255) 
-    board_positions = [((i*64)+64, (512-j*64)+64) for i in range(8) for j in range(1,9)]
+    colour = (100,100,100) 
+    board_positions = [((i*64), (j*64)) for i in range(1,9) for j in range(1,9)]
     
-    w_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\queen_no_bg.png")
-    b_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\b_queen_no_bg.png")
+    w_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\pawn_no_bg.png")
+    b_pawn_img = pygame.image.load(r"C:\Users\User\Documents\GitHub\firstrepo\project_work\sprites\no_backgrounds\b_pawn_no_bg.png")
     
     # Define the dimensions of screen object
     screen = pygame.display.set_mode((640, 640))
     # TITLE OF CANVAS 
     pygame.display.set_caption("The Board") 
+    
+    clock = pygame.time.Clock()
+    test_surface = pygame.Surface((100, 200))
+    test_surface.fill((250, 50, 50))
     exit = False
     
     ## running the game
     while not exit: 
+        
+        # draw all elements
         screen.fill(colour)
-        # for p in board_positions[::2]:
-        #     screen.blit(w_pawn_img, p)
-        # board set up (initial positions)
-        for p in board_positions:
-            screen.blit(b_pawn_img, p)
+
+        screen.blit(test_surface, (64, 64))
+        
+        # update everything
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 exit = True
+            if event.type == pygame.KEYDOWN:
+                screen.blit(b_pawn_img, board_positions[i])
+                i += 1
         pygame.display.update() 
+        clock.tick(60)
     
     
