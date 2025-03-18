b218ffc55a04f3588352e91dc77a4a5123ec4e33
J-Spellacy <judespellacy@gmail.com>
2025-03-18 21:52:45 +0000

    started work on bare_build for numpy ai class for later work

--- project_work/bare_build.py
+++ project_work/bare_build.py
@@ -1,42 +1,48 @@
-## from https://medium.com/towards-data-science/math-neural-network-from-scratch-in-python-d6da9f29ce65
-# Base Class
+## imports
 
-class layer:
+import numpy as np
+
+## to do
+
+# create classes in order to contribute to final class of NN
+
+
+## bugs
+
+
+## classes and functions
+
+class Neuron:
     def __init__(self):
-        self.input = None
+        self.activation = None
+        self.weight_prev = None
+        self.bias_prev = None
+        self.weight_next = None
+        self.bias_next = None
+        
+
+class Layer:
+    def __init__(self, size: int):
+        self.nodes =  np.zeros((size, 1))
+        for i in range(size):
+            self.nodes[i][0] = Neuron
         self.output = None
         
     # computes Y of a layer for a given X at that layer
     def forward_prop(self, input):
-        raise NotImplementedError
+        # matrix multiply with weights and biases from previous layer
+        output = None
+        return output
     
     # computes dE/dX for a given DE/dY and updates parameters
     def backward_prop(self, output_error, learning_rate):
         raise NotImplementedError
     
+## main function
 
-import numpy as np
+def main():
+    pass
 
-# inherit from base class Layer
-class FCLayer(Layer):
-    # input_size = number of input neurons
-    # output_size = number of output neurons
-    def __init__(self, input_size, output_size):
-        self.weights = np.random.rand(input_size, output_size) - 0.5
-        self.bias = np.random.rand(1, output_size) - 0.5
-        
-    # returns output for a given input
-    def forward_prop(self, input_data):
-        self.input = input_data
-        self.output = np.dot(self.input, self.weights) + self.bias
-        return self.output
-    
-    # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
-    def backward_prop(self, output_error, learning_rate):
-        input_error = np.dot(output_error, self.weights.T)
-        weights_error = np.dot(self.input.T, output_error)
-        
-        # update parameters
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
-        return input_error
+## running code
+if __name__ == 'main':
+    main()
